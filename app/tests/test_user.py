962facc89fbbcd1b2797c3b3768ba8658bfaeb8c
root <root@localhost.localdomain>
2019-01-16 14:57:56 +0300

    [start #163249050] add test file for user registration

---
+++ app/tests/test_user.py
@@ -0,0 +1,12 @@
+import unittest
+import json
+from app import create_app
+
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__=='__main__':
+    unittest.main()

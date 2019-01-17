39cc49537ad95694ef7ba512097e8cd0302e95d3
root <root@localhost.localdomain>
2019-01-17 18:36:20 +0300

    [chore #163249372] add tables and create db for test and development

--- app/api/models/db.py
+++ app/api/models/db.py
@@ -1,25 +1,32 @@
+import os
 import psycopg2
+from instance.config import app_config
+
 
 tables = [
             'users',
             'meetups',
             'questions',
-            'comments'
+            'comments',
+            'sessions',
+            'rsvp',
+
+
         ]
 
 table_queries = [
+
             """
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY NOT NULL,
-                firstname VARCHAR(250) NOT NULL,
-                lastname VARCHAR(250) NOT NULL,
-                othername VARCHAR(250) NULL,
-                username VARCHAR(250) NOT NULL,
-                phonenumber VARCHAR(250) NULL,
-                email VARCHAR(250) NOT NULL,
-                password VARCHAR(250) NOT NULL,
-                registered TIMESTAMP WITHOUT TIME ZONE \
-                DEFAULT (NOW() AT TIME ZONE 'utc'),
+                firstname VARCHAR(255) NOT NULL,
+                lastname VARCHAR(255) NOT NULL,
+                othername VARCHAR(255) NULL,
+                username VARCHAR(255) UNIQUE NOT NULL,
+                phonenumber VARCHAR(255) NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                registered TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
                 admin BOOLEAN NOT NULL DEFAULT FALSE
             )
             """,
@@ -27,16 +34,14 @@ table_queries = [
             """
             CREATE TABLE IF NOT EXISTS meetups (
                 id SERIAL PRIMARY KEY NOT NULL,
-                topic VARCHAR(250) NOT NULL,
-                description VARCHAR(250) NOT NULL,
-                location VARCHAR(250) NOT NULL,
-                venue VARCHAR(250) NOT NULL,
+                topic VARCHAR(255) NOT NULL,
+                description VARCHAR(255) NOT NULL,
+                location VARCHAR(255) NOT NULL,
+                venue VARCHAR(255) NOT NULL,
                 time TIMESTAMP WITHOUT TIME ZONE,
                 user_id INTEGER NOT NULL,
-                happening_on TIMESTAMP WITHOUT TIME ZONE \
-                DEFAULT (NOW() AT TIME ZONE 'utc'),
-                created_at TIMESTAMP WITHOUT TIME ZONE \
-                DEFAULT (NOW() AT TIME ZONE 'utc'),
+                happening_on TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
+                created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
                 FOREIGN KEY (user_id) REFERENCES users(id)
 
             )
@@ -45,14 +50,13 @@ table_queries = [
             """
             CREATE TABLE IF NOT EXISTS questions (
                 id SERIAL PRIMARY KEY NOT NULL,
-                topic VARCHAR(250) NULL,
-                body VARCHAR(250) NOT NULL,
+                topic VARCHAR(255) NULL,
+                body VARCHAR(255) NOT NULL,
                 upvotes INTEGER NOT NULL DEFAULT 0,
                 downvotes INTEGER NOT NULL DEFAULT 0,
                 meetup_id INTEGER NOT NULL,
                 user_id INTEGER NOT NULL,
-                created_at TIMESTAMP WITHOUT TIME ZONE \
-                DEFAULT (NOW() AT TIME ZONE 'utc'),
+                created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
                 FOREIGN KEY (meetup_id) REFERENCES meetups(id),
                 FOREIGN KEY (user_id) REFERENCES users(id)
             )
@@ -61,21 +65,48 @@ table_queries = [
             """
             CREATE TABLE IF NOT EXISTS comments (
                 id SERIAL PRIMARY KEY NOT NULL,
-                body VARCHAR(250) NULL,
+                body VARCHAR(255) NULL,
                 question_id INTEGER NOT NULL,
                 user_id INTEGER NOT NULL,
-                created_at TIMESTAMP WITHOUT TIME ZONE \
-                DEFAULT (NOW() AT TIME ZONE 'utc'),
+                created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
                 FOREIGN KEY (question_id) REFERENCES questions(id),
                 FOREIGN KEY (user_id) REFERENCES users(id)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS sessions (
+                id SERIAL PRIMARY KEY NOT NULL,
+                token VARCHAR(255) NOT NULL,
+                created_on TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc')
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS rsvp (
+                id SERIAL PRIMARY KEY NOT NULL,
+                meetup_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
+                response VARCHAR(255) NULL,
+                created_on TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
+                FOREIGN KEY (user_id) REFERENCES users(id),
+                FOREIGN KEY (meetup_id) REFERENCES meetups(id)
+
             )
             """
         ]
 
 
 class Database:
-    def __init__(self):
-        pass
+
+    def __init__(self, env):
+        self.config_name = env
+        self.config = app_config[self.config_name]
+        self.db_user = self.config.DB_USER
+        self.db_password = self.config.DB_PASSWORD
+        self.db_host = self.config.DB_HOST
+        self.db_port = self.config.DB_PORT
+        self.db_name = self.config.DB_NAME
+
+        self.connection = self.connect(self.db_user, self.db_password, self.db_host, self.db_port, self.db_name)
 
     def connect(self, user, password, host, port, db):
         try:
@@ -90,44 +121,46 @@ class Database:
             return "Error while connecting to PostgreSQL", error
 
     def delete_data(self):
-        connection = self.connect("bochie","jamohsize","127.0.0.1","5432","questioner")
 
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
         cursor.execute('TRUNCATE TABLE ' + ','.join(tables) + ' CASCADE')
-        connection.commit()
+        self.connection.commit()
 
     def delete_table_data(self, table_name):
-        connection = self.connect("bochie","jamohsize","127.0.0.1","5432","questioner")
 
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
         cursor.execute('TRUNCATE TABLE ' + table_name + ' CASCADE')
-        connection.commit()
+        self.connection.commit()
 
 
     def drop_tables(self):
-        connection = self.connect("bochie","jamohsize","127.0.0.1","5432","questioner")
 
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
         for table in tables:
             cursor.execute('DROP TABLE IF EXISTS {} CASCADE'.format(table))
 
-        connection.commit()
+        self.connection.commit()
 
     def drop_table(self, table_name):
-        connection = self.connect("bochie","jamohsize","127.0.0.1","5432","questioner")
 
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
 
         cursor.execute('DROP TABLE IF EXISTS {} CASCADE'.format(table_name))
 
-        connection.commit()
+        self.connection.commit()
 
 
     def create_tables(self):
-        connection = self.connect("bochie","jamohsize","127.0.0.1","5432","questioner")
 
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
         for query in table_queries:
             cursor.execute(query)
 
-        connection.commit()
+        self.connection.commit()
+
+    def insert(self, table, data={}):
+
+        cursor = self.connection.cursor()
+        cursor.execute('INSERT INTO {}({}) VALUES ({})'.format(table,key,value))
+
+        self.connection.commit()
--- instance/config.py
+++ instance/config.py
@@ -1,20 +1,29 @@
 import os
 
-class Config(object):
+class Config:
     """Parent configuration class."""
     DEBUG = False
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
 
-
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
+    DB_USER = 'bochie'
+    DB_PASSWORD = 'jamohsize'
+    DB_HOST = '127.0.0.1'
+    DB_PORT = '5432'
+    DB_NAME = 'questioner'
 
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
     DEBUG = True
+    DB_USER = 'bochie'
+    DB_PASSWORD = 'jamohsize'
+    DB_HOST = '127.0.0.1'
+    DB_PORT = '5432'
+    DB_NAME = 'questioner_test'
 
 
 class StagingConfig(Config):
